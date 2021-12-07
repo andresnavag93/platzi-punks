4dab3b6191b1916026ebdae7b863ddf94ec36b86
Andres Navarro <andresnavag93@gmail.com>
2021-12-07 15:56:50 -0400

    :hammer: adding open zeppelin contracts

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+// deploy is an async function
+const deploy = async () => {
+  // getSigners() brings the information we bring from our private key
+  // deployer is an object that allows us to deploy contracts to the network that we have configured
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contract with the account:", deployer.address);
+
+  // We define PlatziPunks in the context
+  const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+  // Instance of the contract deployed
+  const deployed = await PlatziPunks.deploy();
+
+  console.log("Platzi Punks is deployed at:", deployed.address);
+};
+
+// Calling deploy function
+deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
